bce0a5125a45948bd20cde47e922ea8a0090e6a4
Luis Feliciano <justblasey@gmail.com>
2025-05-28 15:52:53 -0400

    Adding code

---
+++ TypeScript/task_2/js/main.ts
@@ -0,0 +1,52 @@
+//task5 Create interface, class and function.
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return "Working from home";
+    }
+    getCoffeeBreak(): string {
+        return "Getting a coffee break";
+    }
+    workDirectorTasks(): string {
+        return "Getting to director tasks";
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return "Working from home";
+    }
+    getCoffeeBreak(): string {
+        return "Getting a coffee break";
+    }
+    workTeacherTasks(): string {
+        return "Getting to work";
+    }
+}
+
+//function that returns Director and has a salary args. If salary is less than 500 it returns a new Teacher
+function createEmployee(salary: number): string {
+    if (salary < 500) {
+        return "Teacher"
+    } else {
+        return "Director"
+    }
+}
+
+//task6 creating functions
+function isDirector(employee: any): employee is Director {
+    return employee.workDirectorTasks !== undefined;
+}
+
+//task6 write function executeWork
